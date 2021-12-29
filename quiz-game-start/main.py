821e3e0d227fe59a32e0b69a74201406e043ff7a
Büşra Boyaci <busraboyaci.brb@gmail.com>
2021-12-30 00:06:27 +0300

    Quiz-Game-OOP

---
+++ quiz-game-start/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+question = QuizBrain(question_bank)
+while question.still_has_questions():
+    question.next_question()
+
+print("You have complated the quiz.")
+print(f"Your final score was: {question.score}/{question.question_number}")
