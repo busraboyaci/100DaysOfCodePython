aa2f01de4de184ddaf700c62936c4166a5f1f814
Büşra Boyaci <busraboyaci.brb@gmail.com>
2022-02-01 14:58:49 +0300

    Refactor_List_Comprehension

--- Turtle/us-states-game-start/main.py
+++ Turtle/us-states-game-start/main.py
@@ -17,10 +17,7 @@ while len(guesses_states) < 50:
     answer_state = screen.textinput(title=f"{len(guesses_states)}/50", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_state = []
-        for state in all_states:
-            if state not in guesses_states:
-                missing_state.append(state)
+        missing_state = [state for state in all_states if state not in guesses_states]
         new_data = pandas.DataFrame(missing_state)
         new_data.to_csv("states_to_learn.csv")
         break
